5dbf0a8f88a17822325216d147b0ec7d41f2abe0
mahesh-madipally <52901849+mahesh-madipally@users.noreply.github.com>
2021-07-22 18:32:36 +0530

    Update Role_12567_dev.tf

--- Role_12567_dev.tf
+++ Role_12567_dev.tf
@@ -3,7 +3,7 @@ source  			= "mahesh-madipally/db_Role_template/google"
 target_level     = "project"
 target_id        = "db-sample-314713"
 role_id          = "Role_12567_dev"
-title            = "sample"
+title            = "Role_12567_dev"
 description 		= "Custom Role Description"
 permissions 		=["container.clusters.get", "container.clusters.list"] 
  }
