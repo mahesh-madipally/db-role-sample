4c6a68a424af686739e125d3a7323c993e529614
mahesh-madipally <52901849+mahesh-madipally@users.noreply.github.com>
2021-07-23 14:16:15 +0530

    Delete Role_dev.tf

--- Role_dev.tf
+++
@@ -1,9 +0,0 @@
-module "project-custom-role" {
-source  			= "mahesh-madipally/db_Role_template/google"
-target_level     = "project"
-target_id        = "db-sample-314713"
-role_id          = "Role_dev"
-title            = "Role_dev"
-description 		= "Custom Role Description"
-permissions 		=["container.clusters.get", "container.clusters.list"] 
- }
