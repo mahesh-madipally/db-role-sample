5c9dc0b62ad67dd1ff2e48b5024394ce77a833b6
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-08-10 14:33:16 +0530

    Created Role script

---
+++ predefined/predefined_role_dev_GKE.tf
@@ -0,0 +1,11 @@
+module  "project-custom-role"{
+source			= "mahesh-madipally/db_Role_template/google"
+target_level     = "project"
+target_id        = "db-sample-314713"
+role_id          = "base_role_devlop"
+title            = "base_role_devlop"
+description 		= "Predefined Role Description"
+permissions 		=["iam.roles.list", "iam.roles.create"]
+base_roles       =["roles/iam.serviceAccountAdmin"]
+
+ }
