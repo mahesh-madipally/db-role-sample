1feaca2026ffbe07dbe33836867b6aaab8dd3353
Ruchita-Khanna <84071208+Ruchita-Khanna@users.noreply.github.com>
2021-08-19 12:22:10 +0530

    Created Role script

---
+++ role_dev_GKE.tf
@@ -0,0 +1,10 @@
+module  "project-custom-role"{
+source			= "mahesh-madipally/db_Role_template/google"
+target_level     = "project"
+target_id        = "db-sample-314713"
+role_id          = "role_develop"
+title            = "role_develop"
+description 		= "Custom Role Description"
+permissions 		=["iam.roles.list", "iam.roles.create"]
+
+ }
