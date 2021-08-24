816657e762c1a0795ae57fa581ef02b7a38f3179
Ruchita-Khanna <84071208+Ruchita-Khanna@users.noreply.github.com>
2021-08-24 12:01:52 +0530

    Created Role script

---
+++ predefined/predefined_role_dev_GKE.tf
@@ -0,0 +1,11 @@
+module  "project-custom-role"{
+source			= "mahesh-madipally/db_Role_template/google"
+target_level     = "project"
+target_id        = "sample-poc-321912"
+role_id          = "predefined_role"
+title            = "predefined_role"
+description 		= "Predefined Role Description"
+permissions 		=["iam.roles.list", "iam.roles.create"]
+base_roles       =["roles/container.clusterAdmin"]
+excluded_permissions =  		["container.clusters.delete", "resourcemanager.projects.list"] 
+ }
