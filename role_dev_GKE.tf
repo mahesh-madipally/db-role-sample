b4e1546c2436a1d250e10f36bfc51e227f7d8144
Ruchita-Khanna <84071208+Ruchita-Khanna@users.noreply.github.com>
2021-08-24 11:57:08 +0530

    Created Role script

---
+++ role_dev_GKE.tf
@@ -0,0 +1,10 @@
+module  "project-custom-role"{
+source			= "mahesh-madipally/db_Role_template/google"
+target_level     = "project"
+target_id        = "sample-poc-321912"
+role_id          = "custom_role_test"
+title            = "role_test_new"
+description 		= "Custom Role Description"
+permissions 		=["iam.roles.list", "iam.roles.create"]
+
+ }
